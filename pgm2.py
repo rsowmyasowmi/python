b32122d3f72ae32f8098f0744213807d8ccbe725
rsowmyasowmi <41049079+rsowmyasowmi@users.noreply.github.com>
2018-09-26 15:17:26 +0530

    Update pgm2.py

--- pgm2.py
+++ pgm2.py
@@ -1,11 +1,11 @@
-n=int(input("Enter number:"))
-temp=n
+n=int(input("N:"))
+T=n
 rev=0
 while(n>0):
     dig=n%10
     rev=rev*10+dig
     n=n//10
-if(temp==rev):
+if(T==rev):
     print("The number is a palindrome!")
 else:
     print("The number isn't a palindrome!")
