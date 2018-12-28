e6d23d834ee2d0f7e7616ed6cb75ca54a729742e
rsowmyasowmi <41049079+rsowmyasowmi@users.noreply.github.com>
2018-12-28 13:49:37 +0530

    Add files via upload

---
+++ Exno54.py
@@ -0,0 +1,15 @@
+def isPrime(n): 
+if n <= 1 : 
+   return False
+for i in range(2, n): 
+    if n % i == 0: 
+      return False
+  return True
+def printPrime(n): 
+    for i in range(2, n + 1): 
+       if isPrime(i): 
+          print(i, end = " ") 
+if name == "main" : 
+    n = 7
+    printPrime(n) 
+      
